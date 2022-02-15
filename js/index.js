735000649d8af321973614a0d4ae4ffb45dcb339
mni-dev <nazrulislam01531@gmail.com>
2022-02-15 21:37:53 +0600

    js code testing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
+//use function to change heading text
 function headChange(){
     const changeHeading = document.getElementById('top-heading');
-    const headingText = changeHeading.innerText;
     const currentText = 'I AM THE PENGUIN'
     changeHeading.innerText = currentText;
     console.log(currentText)
     return currentText;
 }
+
+//log an element using querySelector method
+const findIt =document.querySelector(".fw-bolder");
+console.log(findIt);
+const showIt =document.querySelectorAll("h1");
+console.log(showIt);
